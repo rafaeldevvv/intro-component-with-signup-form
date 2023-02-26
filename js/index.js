c8e5b2b8bdf95d7b92a3d5ddd80e7d76523ea24f
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2023-02-26 14:06:07 -0300

    Check system

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const emailRegExp = /^[\w\d]+@\w+\.\w{2,3}$/;
+
+document.querySelector("form").onsubmit = (e) => {
+  e.preventDefault();
+
+  let okay;
+
+  for (let child of Array.from(e.target.elements)) {
+    if (child.nodeName != "INPUT") continue;
+
+    okay = child.value;
+
+    child.nextSibling.textContent = okay
+      ? ""
+      : child.placeholder + " cannot be empty";
+
+    child.classList.add(okay ? "success" : "failure");
+    child.classList.remove(okay ? "failure" : "success");
+  }
+
+  let emailField = e.target.elements.email;
+  let email = emailField.value;
+
+  okay = emailRegExp.test(email);
+
+  emailField.nextSibling.textContent = okay
+    ? ""
+    : "It looks like an invalid email";
+
+  emailField.classList.add(okay ? "success" : "failure");
+  emailField.classList.remove(okay ? "failure" : "success");
+};
